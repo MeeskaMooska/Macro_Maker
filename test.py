d7d4c41e5ccbd6d1e017246af7c09610b8061488
MeeskaMooska <77514959+MeeskaMooska@users.noreply.github.com>
2023-02-20 20:35:58 -0500

    fixed issues with EditorView, test2 contains the code necessary to make EditorView work and test is the root window.

--- test.py
+++ test.py
@@ -1,7 +1,43 @@
-method = [True, False, True]
-test_list = [[None, None, None], [0, 1, 2]]
-print(test_list[int(method[0])][0])
+from tkinter import *
+import test2
+import threading
+import time
+
+class TemporaryData:
+    def __init__(self):
+        self.self = self
+        self.thread_listener = None
+
+temporary_data = TemporaryData()
+test_data = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14], [15, 16, 17, 18, 19],
+                           [20, 21, 22, 23, 24]]
+editor_view = test2.EditorView()
 
 
-# test_list[int(method[0])][0]
-# mouse_input_list[int(self.method[0])][0]
+def editor_view_listener():
+    print("sdf")
+    while editor_view.running == True:
+        time.sleep(1)
+        print(editor_view.running)
+    else:
+        print(editor_view.running)
+
+
+def start_new_gui():
+    if editor_view.running:
+        print("there is already an instance of editor view running, close that before continuing.")
+    else:
+        editor_view.logging_data = test_data
+        editor_view.running = True
+        temporary_data.thread_listener = threading.Thread(target=editor_view_listener)
+        temporary_data.thread_listener.start()
+        editor_view.setup_gui()
+
+
+
+root = Tk()
+
+open_nw_button = Button(root, text="open new window", command=start_new_gui)
+open_nw_button.pack()
+
+root.mainloop()
--- test2.py
+++ test2.py
@@ -1,23 +1,91 @@
 from tkinter import *
-from pynput.keyboard import Key, Listener
-
-def on_press(key):
-    print("s")
-
-def on_release(key):
-    print("d")
 
 
-keyListener = Listener(on_press=on_press, on_release=on_release)
+class EditorView:
+    def __init__(self):
+        self.self = self
+        self.root = None
+        self.logging_data = None
+        self.selected_index = None
+        self.running = False
 
+        # Declaring all objects
+        # Listbox
+        self.regular_key_listbox = None
+        self.special_key_listbox = None
+        self.mouse_click_listbox = None
+        self.mouse_move_listbox = None
+        self.mouse_scroll_listbox = None
+        self.listbox_list = None
 
-root = Tk()
-button = Button(root, text="+")
-button.pack()
-root.mainloop()
+        # Buttons and Labels
+        self.selected_data_label = None
+        self.edit_button = None
+        self.delete_selected_button = None
+        self.start_time_entry = None
+        self.duration_entry = None
+        self.save_button = None
+        self.delete_button = None
 
-keyListener.start()
-'''def fingers_crossed():
-    keyListener.join()
+    def setup_gui(self):
+        # Root settings
+        self.root = Tk()
+        self.root.resizable(False, False)
+        self.root.title("Editor View")
+        self.root.protocol("WM_DELETE_WINDOW", self.closed)
 
-fingers_crossed()'''
+        # Listbox settings
+        self.regular_key_listbox = Listbox(self.root)
+        self.special_key_listbox = Listbox(self.root)
+        self.mouse_click_listbox = Listbox(self.root)
+        self.mouse_move_listbox = Listbox(self.root)
+        self.mouse_scroll_listbox = Listbox(self.root)
+        self.listbox_list = [self.regular_key_listbox, self.special_key_listbox, self.mouse_click_listbox,
+                             self.mouse_move_listbox, self.mouse_scroll_listbox]
+        self.config_listbox()
+        for count, title in enumerate(["Regular Keys", "Special Keys", "Mouse Click", "Mouse Move", "Mouse Scroll"]):
+            Label(self.root, text=title).grid(row=0, column=count)
+        for count, obj in enumerate(self.listbox_list):
+            self.listbox_list[count].bind('<<ListboxSelect>>', self.selected_data)
+            self.listbox_list[count].grid(row=1, column=count, padx=5)
+
+        # Buttons and Labels
+        Label(self.root, text="Selected Data:").grid(row=2, column=0)
+        self.selected_data_label = Label(self.root)
+        self.edit_button = Button(self.root, text="Edit Selected").grid(row=3, column=0)
+        self.delete_selected_button = Button(self.root, text="Delete Selected", command=self.delete_selected).grid(
+            row=4, column=0)
+        Label(self.root, text="Start Time:").grid(row=3, column=1)
+        Label(self.root, text="Duration:").grid(row=4, column=1)
+        self.start_time_entry = Entry(self.root)
+        self.duration_entry = Entry(self.root)
+        self.start_time_entry.grid(row=3, column=2)
+        self.duration_entry.grid(row=4, column=2)
+        self.save_button = Button(self.root, text="Save Macro", command=self.save_macro).grid(row=3, column=4)
+        self.delete_button = Button(self.root, text="Delete Macro").grid(row=4, column=4)
+
+        # Starts the mainloop
+        self.root.mainloop()
+
+    def config_listbox(self):
+        for count, data_list in enumerate(self.logging_data):
+            for index, data in enumerate(data_list):
+                self.listbox_list[count].insert(index, data)
+
+    def selected_data(self, event):
+        for count, listbox in enumerate(self.listbox_list):
+            if listbox.curselection() != ():
+                self.selected_index = [self.listbox_list.index(listbox), listbox.curselection()[0]]
+                self.selected_data_label.config(text=str(self.logging_data[self.selected_index[0]][self.selected_index[1]]))
+                break
+
+    def delete_selected(self):
+        self.listbox_list[self.selected_index[0]].delete(self.selected_index[1])
+        self.logging_data[self.selected_index[0]].pop(int(self.selected_index[1]))
+
+    def save_macro(self):
+        pass
+
+    def closed(self):
+        self.root.destroy()
+        self.__init__()
